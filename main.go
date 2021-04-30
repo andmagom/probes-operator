f2ca761ea647a38459e13851db6c3554438a1a24
Andrés M. Gómez <andres86mauricio@hotmail.com>
2021-04-29 19:07:26 -0500

    Controller added to main.go

--- main.go
+++ main.go
@@ -30,7 +30,11 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
 	//+kubebuilder:scaffold:imports
+
+	cachev1alpha1 "github.com/example/probes-operator/api/v1alpha1"
+	"github.com/example/probes-operator/controllers"
 )
 
 var (
@@ -41,6 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(cachev1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +79,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ProbesCheckerReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("ProbesChecker"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ProbesChecker")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
